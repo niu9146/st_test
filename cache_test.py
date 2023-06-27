c0ed9f21f826a0c4e5cc8f3f707f122eec55abce
Goyo <goyodiaz@gmail.com>
2023-06-28 00:15:11 +0200

    More expensive query and Rerun button.

--- cache_test.py
+++ cache_test.py
@@ -4,15 +4,16 @@ import streamlit as st
 
 @st.cache_data(show_spinner=True)
 def get_answer_to_life_universe_and_everything():
-    time.sleep(5)
+    time.sleep(10)
     return 42
 
 
 def main():
     data = get_answer_to_life_universe_and_everything()
-    st.markdown(f"The Answer to the Ultimate Question of Life, The Universe, and Everything is **{data}**")
+    st.markdown(f"The Answer to the Ultimate Question of Life, The Universe, and Everything is **{data}**.")
     if st.button("Clear cache"):
         st.cache_data.clear()
+    st.button("Rerun")
 
 
 if __name__ == "__main__":
