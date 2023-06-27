b4efe84b69b7635946f93abbfa49a89ee1ba9696
Goyo <goyodiaz@gmail.com>
2023-06-28 00:08:31 +0200

    Fix syntax error.

--- cache_test.py
+++ cache_test.py
@@ -10,7 +10,7 @@ def get_answer_to_life_universe_and_everything():
 
 def main():
     data = get_answer_to_life_universe_and_everything()
-    st.markdown(f"The Answer to the Ultimate Question of Life, The Universe, and Everything is **{}**")
+    st.markdown(f"The Answer to the Ultimate Question of Life, The Universe, and Everything is **{data}**")
     if st.button("Clear cache"):
         st.cache_data.clear()
 
